8be45bd01552c55e486bd0e9b47ab18501684896
Ante Brozović <47977690+aposhtol@users.noreply.github.com>
2022-12-22 18:26:22 +0100

    Add files via upload

---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3002/anecdotes'
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const newAnecdote = async (content) => {
+  const object = { content, votes: 0 }
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
+
+const updateAnecdote = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject)
+  return response.data
+}
+
+export default { getAll, newAnecdote, updateAnecdote }
